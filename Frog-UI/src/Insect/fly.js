c3acc7be0a9a3544694d55f180e80d9129dcd061
luanm <mll2071@rit.edu>
2024-11-02 20:28:06 -0400

    Made rotation of btns and removed background

--- Frog-UI/src/Insect/fly.js
+++ Frog-UI/src/Insect/fly.js
@@ -5,21 +5,28 @@ class Fly {
         this.width = window.innerWidth;
         this.x = Math.floor(Math.random() * this.height);
         this.y = Math.floor(Math.random() * this.width);
-        this.id = `fly-${Date.now()}`
-        //this.id = "fly"
+        this.id = `fly-${Date.now()}` // make unique IDs for each btn
     }
     killFly() {
         const button = document.getElementById(this.id);
         button.parentNode.removeChild(button);
     }
     createButton() {
-        const button = document.createElement("button");
-        button.innerHTML = "Fly";
+        const button = document.createElement("button"); // create btn
+        //button.innerHTML = "Fly";
         button.id = this.id
         button.onclick = () => this.killFly();
-        button.style.position = "absolute";
+        button.style.position = "absolute"; // btn can be placed anywhere
+        button.className = "flies"
+        const image = document.createElement("img")
+        image.src = "../../../Assets/fly.png"
+        image.style.width = "20px"; // Set image width
+        image.style.height = "20px"; 
         button.style.top = `${this.x*0.9}px`; // Set the random x-coord
         button.style.left = `${this.y*0.9}px`; // Set the random y-coord
+        button.appendChild(image)
+        const rotation = Math.floor(Math.random() * 360); // Random rotation from 0 to 359 degrees
+        button.style.transform = `rotate(${rotation}deg)`;
         document.body.appendChild(button);
         return button;
     }
